d46328f05646689e6f57265d65ce4896b3bf37f3
k3srpn <k3srpn@gmail.com>
2023-11-03 23:36:56 +0900

    Attach module to app module

--- archieve/nestjs/src/app.module.ts
+++ archieve/nestjs/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { QuaggaModule } from './providers/quagga/quagga.module';
+import { S3Module } from './providers/s3/s3.module';
+import { SqsModule } from './providers/sqs/sqs.module';
 
 @Module({
-  imports: [],
+  imports: [QuaggaModule, S3Module, SqsModule],
   controllers: [],
   providers: [],
 })
